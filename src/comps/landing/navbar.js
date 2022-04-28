5cdf8c662bc3ee0a562eecbf3c2cf1c1d5845c89
Csaba Králik <kralikcsabi@gmail.com>
2022-04-28 18:10:48 +0200

    fix deploy

--- src/comps/landing/navbar.js
+++ src/comps/landing/navbar.js
@@ -1,19 +1,23 @@
 import "./navbar.css";
 import { Link } from "react-router-dom";
 function Navbar(props) {
+  booking = false;
+  if (props.booking === "true") {
+    booking = true;
+  }
   return (
     <header id="navbar">
-      {props.booking ? (
+      {booking ? (
         <a href="Book-a-table#Landing">Home</a>
       ) : (
         <a href="#Landing">Home</a>
       )}
-      {props.booking ? (
+      {booking ? (
         <a href="Book-a-table#Intro">Introduction</a>
       ) : (
         <a href="#Intro">Introduction</a>
       )}
-      {props.booking ? (
+      {booking ? (
         <a href="Book-a-table#Menu">Menu</a>
       ) : (
         <a href="#Menu">Menu</a>
